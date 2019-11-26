090a85bc8a21ca8ce363a59621d6bc8ac35b5b50
Sergio <281540+sergio97@users.noreply.github.com>
2019-11-26 13:43:09 -0500

    Update python-interpreter-setup.py

--- python-interpreter-setup.py
+++ python-interpreter-setup.py
@@ -3,28 +3,52 @@
 import os
 import re
 import sys
+import copy
 import json
+import pytz
 import mock
+import time
 import uuid
-import bleach
-import random
 import string
+import random
 import couchdb
-import decimal
 import logging
 import requests
 import functools
 import itertools
-from pprint import pprint
+import simplejson
+import collections
+
+import urllib.parse
+q = quote = urllib.parse.quote
+u = uq = unquote = urllib.parse.unquote
+
+import decimal
 from decimal import Decimal
+D = Decimal
+
+import datetime
 from datetime import datetime, timedelta
 
+from pprint import pprint, pformat
 pp = pprint
-D = Decimal
 
 logging.getLogger().addHandler(logging.StreamHandler())
 LOG = logger = logging.getLogger(__name__)
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+LOG.addHandler(ch)
 
-def nicely(serializable_object):
-    print(json.dumps(serializable_object, indent=4))
+nicely = lambda x: print(simplejson.dumps(x, indent=4, sort_keys=True))
+def nicely2(serializable_object):
+    print(simplejson.dumps(serializable_object, indent=4, sort_keys=True))
+
+def generate_string(length=10, source=string.ascii_lowercase):
+    return ''.join(random.choice(source) for x in range(length))
+
+gs = generate_string
+gsp = functools.partial(generate_string, source=string.printable)
+
 
